0339328cce05a32d533141b42f85d2a57dbbae1a
David Bujosa <davidbujosa@gmail.com>
2022-03-26 20:32:06 +0100

    feat: add fact class

---
+++ app/src/main/java/com/bujosa/rigel/Fact.java
@@ -0,0 +1,20 @@
+package com.bujosa.rigel;
+
+public class Fact {
+
+    public static String[] getFacts() {
+        return new String[]{
+
+                "Naruto Uzumaki",
+                "One Piece",
+                "Bleach",
+                "Sao",
+                "Dragon Ball",
+                "Attack of titans",
+                "Pokemon",
+                "Tensei Shitara",
+                "Black Clover",
+                "Fairy Tail"
+        };
+    }
+}
