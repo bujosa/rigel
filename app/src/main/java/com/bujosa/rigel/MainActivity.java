df51603250eb26131b11a1b30975776403446c2e
David Bujosa <davidbujosa@gmail.com>
2022-03-26 20:37:35 +0100

    feat: add scroll view

--- app/src/main/java/com/bujosa/rigel/MainActivity.java
+++ app/src/main/java/com/bujosa/rigel/MainActivity.java
@@ -3,12 +3,25 @@ package com.bujosa.rigel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView factTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        factTextView = findViewById(R.id.txt_fact);
+
+        String[] myFacts = Fact.getFacts();
+
+        for (String fact: myFacts){
+            factTextView.append(fact + "\n\n");
+        }
+
+
     }
 }
